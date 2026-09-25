2f0322a7d9b7d7256b952f9762a14da8d9ad556b
gabriel-pinho1 <gabmhpinho@gmail.com>
2026-09-25 18:25:15 +0100

    versao com mais coisas

--- trainning.py
+++ trainning.py
@@ -10,32 +10,34 @@ from sklearn.metrics import f1_score
 
 digits=load_digits()
 k=0
-k_max = 10
+k_max = 20
 score_best = 0
 f1_best = 0
 k_f1 = 0
 k_best = 0
 X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.33, random_state=42) #Separate the data into training and test for model evaluation
+accuracies = []
+f1_scores = []
+k_values = range(1,k_max)
+for k in k_values:
 
-for i in range(1,k_max):
-
-    k=i
     knn=KNeighborsClassifier(n_neighbors=k)
     knn.fit(X_train,y_train)
     y_pred=knn.predict(X_test)
-    scores=metrics.accuracy_score(y_test,y_pred)
-    f1_scores = metrics.f1_score(y_test, y_pred, average='macro')
-    # print(f" K ={k} accuracy ={scores}")
-    if score_best< scores:
-        score_best = scores
-        k_best = k
+    acc_scores=metrics.accuracy_score(y_test,y_pred)
+    f1 = metrics.f1_score(y_test, y_pred, average='macro')
+    accuracies.append(acc_scores)
+    f1_scores.append(f1)
 
-    if f1_best < f1_scores:
-            f1_best = f1_scores
-            k_f1 = k
-print(f"best score {score_best} for {k_best} neighbours")  
-print(f"best f1 score {f1_best} for {k_f1} neighbours")  
+plt.figure(figsize=(10, 6))
+plt.plot(k_values, accuracies, label='Accuracy', marker='o')
+plt.plot(k_values, f1_scores, label='F1 Score', marker='s')
 
+plt.title('k-NN Performance on Test Set')
+plt.xlabel('Number of Neighbors (K)')
+plt.ylabel('Score')
+plt.xticks(k_values)
+plt.legend()
+plt.grid(True)
 
-
-
+plt.savefig('knn_metrics.png')
