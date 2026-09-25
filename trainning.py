3003777fe162a4b399675e4b253319479275cc7d
gabriel-pinho1 <gabmhpinho@gmail.com>
2026-09-25 17:53:35 +0100

    added f1 macro metric

--- trainning.py
+++ trainning.py
@@ -6,12 +6,14 @@ from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
-from sklearn.model_selection import cross_val_score
+from sklearn.metrics import f1_score
 
 digits=load_digits()
 k=0
 k_max = 10
 score_best = 0
+f1_best = 0
+k_f1 = 0
 k_best = 0
 X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.33, random_state=42) #Separate the data into training and test for model evaluation
 
@@ -22,11 +24,18 @@ for i in range(1,k_max):
     knn.fit(X_train,y_train)
     y_pred=knn.predict(X_test)
     scores=metrics.accuracy_score(y_test,y_pred)
+    f1_scores = metrics.f1_score(y_test, y_pred, average='macro')
     # print(f" K ={k} accuracy ={scores}")
     if score_best< scores:
         score_best = scores
         k_best = k
-print(f"best score{score_best} for {k_best}")  
+
+    if f1_best < f1_scores:
+            f1_best = f1_scores
+            k_f1 = k
+print(f"best score {score_best} for {k_best} neighbours")  
+print(f"best f1 score {f1_best} for {k_f1} neighbours")  
+
 
 
 
