254d4a1c6b8412b7956e76ecd612bd153450c07c
gabriel-pinho1 <gabmhpinho@gmail.com>
2026-09-25 17:14:36 +0100

    random seed testing wild range of test split

--- trainning.py
+++ trainning.py
@@ -1,29 +1,42 @@
-import numpy as np;
-import matplotlib.pyplot as plt;
-import pickle;
-import pandas as pd;
-import pandas as pd;
-from sklearn.datasets import load_digits;
-from sklearn.model_selection import train_test_split;
-from sklearn.neighbors import KNeighborsClassifier;
-from sklearn import metrics;
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import pandas as pd
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
 
 digits=load_digits()
-X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.33, random_state=42) #Separate the data into training and test for model evaluation
-score_best = 0
-k_best = 1
+k=0
+k_max = 10
 
-for i in range(1,1203):
+k_statistics = {'k_number' : range(0,k_max),
+                'best_result_count' : 0
+                
+                }
+df = pd.DataFrame.from_dict(k_statistics)
 
-    k=i
-    knn=KNeighborsClassifier(n_neighbors=k)
-    knn.fit(X_train,y_train)
-    y_pred=knn.predict(X_test)
-    scores=metrics.accuracy_score(y_test,y_pred)
-    print(f" K ={k} accuracy ={scores}")
-    if score_best< scores:
-        score_best = scores
-        k_best = k
-    print(f"best score{score_best} for {k_best} neighbours")
+for j  in range (25, 51): 
+    size = j/100
+    for n in range (1,50):
+        X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=size, random_state=n) #Separate the data into training and test for model evaluation
+        score_best = 0
+        k_best = 1
+        for i in range(1,k_max):
 
-  
+            k=i
+            knn=KNeighborsClassifier(n_neighbors=k)
+            knn.fit(X_train,y_train)
+            y_pred=knn.predict(X_test)
+            scores=metrics.accuracy_score(y_test,y_pred)
+            # print(f" K ={k} accuracy ={scores}")
+            if score_best< scores:
+                score_best = scores
+                k_best = k
+        print(f"best score{score_best} for {k_best} neighbours for test_size: {size}")  
+        # store result distribution
+        df.loc[k_best, 'best_result_count'] += 1
+
+
